d4426453a0f996857781f2dd35fffaa4ded76b10
mohammedichou <mohammedichou@MacBook-Pro-de-mohammed.local>
2023-02-15 14:04:02 +0100

    ajout de card de formation

--- src/views/formation/Formation.js
+++ src/views/formation/Formation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Grid, Box } from '@mui/material';
 import PageContainer from 'src/components/container/PageContainer';
+import CardFormation from 'src/views/formation/components/CardFormation'
 
 
 const Formation = () => {
   return (
     <PageContainer title="Formation" description="this is Formation">
       <Box>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {Array.from(Array(6)).map((_, index) => (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                    <CardFormation></CardFormation>
+                </Grid>
+            ))}
+        </Grid>
+        
       </Box>
     </PageContainer>
   );
